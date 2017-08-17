aeb3928d1a2da33b29daae7d168a01aab93ce97c
Virginia Chen <virginia.chen32@gmail.com>
2017-08-17 10:45:17 -0700

    Heaps exercise

--- W12D3_heaps/virginia_chen/lib/heap.rb
+++ W12D3_heaps/virginia_chen/lib/heap.rb
@@ -2,30 +2,80 @@ class BinaryMinHeap
   attr_reader :store, :prc
 
   def initialize(&prc)
+    @prc = prc
+    unless prc
+      @prc = Proc.new{ |x,y| x <=> y }
+    end
+    @store = []
+
   end
 
   def count
+    @store.length
   end
 
   def extract
+    extracted = @store[0]
+    @store[0] = @store[count-1]
+    @store.pop
+    self.class.heapify_down(@store, 0)
+    extracted
   end
 
   def peek
+    @store[0]
   end
 
   def push(val)
+    @store.push(val)
+    self.class.heapify_up(@store, @store.length-1)
   end
 
   public
   def self.child_indices(len, parent_index)
+    children = []
+    if 2*(parent_index)+ 1 < len
+      children.push(2*(parent_index)+ 1)
+    end
+    if 2*(parent_index)+ 2 < len
+      children.push(2*(parent_index)+ 2)
+    end
+    children
   end
 
   def self.parent_index(child_index)
+    if child_index == 0
+      raise "root has no parent"
+    else
+      (child_index - 1)/2
+    end
   end
 
   def self.heapify_down(array, parent_idx, len = array.length, &prc)
+      prc ||= Proc.new{ |x,y| x <=> y }
+      children_indices = self.child_indices(len, parent_idx)
+      min_child_idx = children_indices.min { |idx| array[idx]}
+      if min_child_idx
+        if prc.call(array[parent_idx],array[min_child_idx]) > 0
+          array[parent_idx], array[min_child_idx] = array[min_child_idx], array[parent_idx]
+          self.heapify_down(array, min_child_idx, len = array.length, &prc)
+        end
+      else
+        return array
+      end
   end
 
   def self.heapify_up(array, child_idx, len = array.length, &prc)
+    prc ||= Proc.new{ |x,y| x <=> y }
+
+    if child_idx > 0
+      parent_idx = self.parent_index(child_idx)
+      if prc.call(array[child_idx],array[parent_idx]) < 0
+        array[child_idx], array[parent_idx] = array[parent_idx], array[child_idx]
+        self.heapify_up(array,parent_idx,len = array.length, &prc)
+      end
+    else
+      return array
+    end
   end
 end
--- W12D3_heaps/virginia_chen/lib/heap_sort.rb
+++ W12D3_heaps/virginia_chen/lib/heap_sort.rb
@@ -1,6 +1,34 @@
 require_relative "heap"
+require "byebug"
 
 class Array
   def heap_sort!
+    # idx = 0
+    # array = BinaryMinHeap.new
+    # while idx < self.length
+    #   p 'the array'
+    #   p self
+    #   BinaryMinHeap.heapify_up(self, idx) {|x,y| x <=> y }
+    #   idx += 1
+    # end
+    # j = 0
+    # last_idx = self.length-1
+    # while last_idx > 0
+    #   self[0],self[last_idx] = self[last_idx], self[0]
+    #   last_idx -= 1
+    #   BinaryMinHeap.heapify_down(self[0..last_idx], 0)
+    # end
+    heaped = BinaryMinHeap.new
+    self.each do |el|
+      p 'heap'
+      p heaped
+      heaped.push(el)
+    end
+    newArr = []
+    until heaped.count == 0
+      newArr.push(heaped.extract)
+    end
+    p newArr
+    self.replace(newArr)
   end
 end
